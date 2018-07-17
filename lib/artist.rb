20d5efb14399703b58c79386dd673b4250235ee2
Ana Bebic <anabebic.x@gmail.com>
2018-07-17 14:15:01 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,15 @@
 class Artist 
   attr_accessor :name, :song  
+  @songs = 0 
   def initialize(name)
+  
     @name =name 
     @song = song 
   end 
   
   def songs(song)
     @songs = self.song
+    @songs +=1 
   end
 end 
   
