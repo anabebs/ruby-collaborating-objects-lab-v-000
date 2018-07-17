4ea843f3d3dc2675fcb55ddb1a973f29fa57e452
Ana Bebic <anabebic.x@gmail.com>
2018-07-17 14:18:01 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,15 +1,38 @@
-class Artist 
-  attr_accessor :name, :song  
-  @songs = 0 
+class Artist
+  attr_accessor :name
+
+  @@all = []
+
   def initialize(name)
-  
-    @name =name 
-    @song = song 
-  end 
-  
-  def songs(song)
-    @songs = self.song
-    @songs +=1 
+    @name = name
+    @songs= []
   end
-end 
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    song
+  end
+
+  def songs
+    @songs
+  end
+
+  def save
+   @@all << self
+    self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_or_create_by_name(name)
+    self.all.detect {|artist| artist.name == name} || Artist.new(name).save
+  end
+
+  def print_songs
+    self.songs.each {|song| puts song.name} 
+   end
+end
   
